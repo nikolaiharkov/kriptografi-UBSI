07ca3c5a2997d62d583d20d883a613112e9b958f
Nikolai Harkov <74813988+nikolaiharkov@users.noreply.github.com>
2022-06-07 22:35:44 +0700

    update about

---
+++ about.php
@@ -0,0 +1,69 @@
+<!-- import bootstrap from /css -->
+<!-- make a table with 2 row -->
+
+<html>
+<head>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <title>Caesar Cipher</title>
+</head>
+<body>
+    <div class="container">
+        <div class="row d-flex justify-content-center mt-5">
+            <div class="col-12 col-md-6 col-lg-6">
+                <div class="card">
+                    <div class="card-header">
+                        <h3>Program dirancang oleh:</h3>
+                    </div>
+                    <div class="card-body pb-3">
+                    <table class="table">
+  <thead class="thead-dark">
+    <tr>
+      <th>Nama</th>
+      <th>NIM</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>Daffa Aslam Multazam</td>
+      <td>17191011</td>
+    </tr>
+    <tr>
+      <td>M. Fathur Rizky</td>
+      <td>17191119</td>
+    </tr>
+    <tr>
+      <td>M. Alvan Fauzan</td>
+      <td>17191231</td>
+    </tr>
+    <tr>
+      <td>Muggi Putra Pamungkas</td>
+      <td>17190889</td>
+    </tr>
+    <tr>
+      <td>Nur Khoirunnisa</td>
+      <td>17191083</td>
+    </tr>
+    <tr>
+      <td>Gani Parlindungan</td>
+      <td>17190901</td>
+    </tr>
+  </tbody>
+</table>
+                           
+                            
+                            <div class="form-group d-flex flex-row">
+                                
+                                <a class="btn btn-warning" onclick="window.location.href='/kriptografi-UBSI'">Home</a>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+</body>
+</html>
